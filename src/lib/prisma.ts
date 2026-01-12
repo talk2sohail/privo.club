3b27cd8666733284c87d2a5ed0179b6643f61014
talk2sohail <talk2sohail100@gmail.com>
2026-01-13 00:11:43 +0530

    anable db migration

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,7 +1,15 @@
 import { PrismaClient } from "@prisma/client";
+import { Pool } from "pg";
+import { PrismaPg } from "@prisma/adapter-pg";
+
+const connectionString = process.env.DATABASE_URL;
+
+const pool = new Pool({ connectionString });
+const adapter = new PrismaPg(pool);
 
 const prismaClientSingleton = () => {
   return new PrismaClient({
+    adapter,
     log: ["query", "info", "warn", "error"],
   });
 };
